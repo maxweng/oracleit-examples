2e84d291fa44779ea54d81327fb585b51e1eed5b
yanghb <yanghb_1@163.com>
2016-11-15 11:56:05 +0800

    example Timer

---
+++ contracts/api/OracleItAPI.sol
@@ -0,0 +1,87 @@
+contract OracleItI {
+    address public callbackAddress;
+    uint public defaultGasLimit;
+    function query(uint timestamp, bytes32 dataSource, string arg1, string arg2, uint gasLimit, uint gasPrice) returns(uint id);
+    function getPrice(bytes32 dataSource) returns(uint price);
+}
+
+
+contract OracleItAddrResolverI {
+    function getAddress() returns (address _oracleItAddress);
+}
+
+
+contract usingOracleIt {
+    OracleItAddrResolverI OIAR;
+    
+    OracleItI oracleIt;
+    modifier oracleItAPI {
+        if(address(OIAR)==0) oracleItSetNetwork();
+        oracleIt = OracleItI(OIAR.getAddress());
+        _
+    }
+
+    function oracleItSetNetwork() internal returns(bool){
+        if (getCodeSize(0x647d90242b287fdbd80558b3d972b25dbb59a081)>0){
+            OIAR = OracleItAddrResolverI(0x647d90242b287fdbd80558b3d972b25dbb59a081);
+            return true;
+        }
+        return false;
+    }
+    
+    function oracleItQuery(string dataSource, string arg1) oracleItAPI internal returns (uint id){
+        return oracleItQuery(0, dataSource, arg1, "", 0, 0);
+    }
+
+    function oracleItQuery(uint timestamp, string dataSource, string arg1) oracleItAPI internal returns (uint id){
+        return oracleItQuery(timestamp, dataSource, arg1, "", 0, 0);
+    }
+
+    function oracleItQuery(string dataSource, string arg1, uint gasLimit, uint gasPrice) oracleItAPI internal returns (uint id){
+        return oracleItQuery(0, dataSource, arg1, "", gasLimit, gasPrice);
+    }
+
+    function oracleItQuery(uint timestamp, string dataSource, string arg1, uint gasLimit, uint gasPrice) oracleItAPI internal returns (uint id){
+        return oracleItQuery(timestamp, dataSource, arg1, "", gasLimit, gasPrice);
+    }
+
+    function oracleItQuery(string dataSource, string arg1, string arg2) oracleItAPI internal returns (uint id){
+        return oracleItQuery(0, dataSource, arg1, arg2, 0, 0);
+    }
+
+    function oracleItQuery(uint timestamp, string dataSource, string arg1, string arg2) oracleItAPI internal returns (uint id){
+        return oracleItQuery(timestamp, dataSource, arg1, arg2, 0, 0);
+    }
+
+    function oracleItQuery(string dataSource, string arg1, string arg2, uint gasLimit, uint gasPrice) oracleItAPI internal returns (uint id){
+        return oracleItQuery(0, dataSource, arg1, arg2, gasLimit, gasPrice);
+    }
+
+    function oracleItQuery(uint timestamp, string _dataSource, string arg1, string arg2, uint gasLimit, uint gasPrice) oracleItAPI internal returns (uint id){
+        if(gasLimit == 0) gasLimit = oracleIt.defaultGasLimit();
+        if(gasPrice == 0) gasPrice = tx.gasprice;
+        uint price = oracleIt.getPrice(stringToBytes32(_dataSource));
+        if (price > 1 ether) return 0; // unexpectedly high price
+        price += gasLimit * gasPrice;
+        return oracleIt.query.value(price)(timestamp, stringToBytes32(_dataSource), arg1, arg2, gasLimit, gasPrice);
+    }
+
+    function oracleItCallbackAddress() oracleItAPI internal returns (address){
+        return oracleIt.callbackAddress();
+    }
+
+    function getCodeSize(address _addr) constant internal returns(uint _size) {
+        assembly {
+            _size := extcodesize(_addr)
+        }
+    }
+
+    function stringToBytes32(string memory source) returns (bytes32 result) {
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+}
+
+contract OracleItAPI{
+}
---
+++ tools/show_timer.js
@@ -0,0 +1,60 @@
+module.exports = function (callback) {
+    var accounts = web3.eth.accounts;
+    
+    var transInt = function(value){return +value};
+    var transUtf8 = function(value){return web3.toUtf8(value)};
+    var transString = function(value){return "" + value};
+    var transEther = function(value){return +web3.fromWei(value, "ether")};
+    var transBool = function(value){return !!value};
+
+    var timer = Timer.deployed();
+
+    function showBalances(cb) {
+        if(typeof(cb) === "undefined") cb = function(){};
+        var timer_balance = web3.fromWei(+web3.eth.getBalance(timer.address), "ether")
+        console.log("Timer Balance: ", timer_balance);
+        cb(timer_balance);
+    }
+    
+    function getVariable(variableName, cb) {
+        timer[variableName].call().then(function (value) {
+            cb(value);
+        }).catch(function(err){
+            console.log(err);
+            process.exit();
+        });
+    }
+    
+    function getInfo(cb) {
+        if(typeof(cb) === "undefined") cb = function(){};
+        var info = {};
+        var variables = [
+            ["time", transInt],
+            ["times", transInt],
+            ["interval", transInt],
+            ["running", transBool],
+        ];
+        var work = function(i, work_cb){
+            if(i >= variables.length){
+                return work_cb();
+            }
+            var veriableName = variables[i][0];
+            var transFunc = variables[i][1];
+            getVariable(veriableName, function(value){
+                info[veriableName] = transFunc(value);
+                i++;
+                work(i, work_cb);
+            });
+        }
+        work(0, function(){
+            console.log("Timer info: ", info);
+            cb(info);
+        });
+    }
+    
+    showBalances();
+    getInfo(function(){
+        process.exit();
+    });
+
+}
