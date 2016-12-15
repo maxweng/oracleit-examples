c984e0ea809b47faa7b3c38ac478b302da7716a4
yanghb <yanghb_1@163.com>
2016-12-15 16:26:52 +0800

    internal funcs

--- contracts/api/OracleItAPI.sol
+++ contracts/api/OracleItAPI.sol
@@ -78,7 +78,7 @@ contract usingOracleIt {
         }
     }
 
-    function stringToBytes32(string memory source) returns (bytes32 result) {
+    function stringToBytes32(string memory source) internal returns (bytes32 result) {
         assembly {
             result := mload(add(source, 32))
         }
